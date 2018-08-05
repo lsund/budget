77308eaa0f5e5a8026741aa2abf62dbf46e8c3be
Ludvig Sundström <lud.sund@gmail.com>
2018-08-05 18:38:54 +0200

    Add Render namespace

--- src/clj/budget/handler.clj
+++ src/clj/budget/handler.clj
@@ -2,8 +2,8 @@
   (:require
    [compojure.route :as r]
    [compojure.core :refer [routes GET POST ANY]]
-   [hiccup.page :refer [html5 include-css include-js]]
-   [hiccup.form :refer [form-to]]
+
+
    [ring.util.response :refer [redirect]]
    [ring.middleware
     [defaults :refer [site-defaults wrap-defaults]]
@@ -15,137 +15,44 @@
    [taoensso.timbre.appenders.core :as appenders]
 
    [budget.db :as db]
-   [budget.util :as u]))
+   [budget.util :as u]
+   [budget.render :as render]))
+
 
 (timbre/merge-config!
  {:appenders
   {:spit (appenders/spit-appender {:fname "data/budget.log"})}})
 
 
-(def avail 927)
-
-(def n-transactions 20)
-
-
-(defn cat-names
-  []
-  (let [cs (db/get-categories)
-        ids (map :id cs)
-        ns  (map :name cs)]
-    (zipmap ids ns)))
-
-(defn fmt-entry
-  [{:keys [name funds]}]
-  (format "%s %s" name funds))
-
-(defn entry
-  [e]
-  (let [label (-> e (select-keys [:name :funds]) fmt-entry)]
-    [:tr
-
-     [:td
-      (form-to  [:post "/update-name"]
-                [:input {:type :hidden :name "cat-id" :value (:id e)}]
-                [:input {:type :text :name "cat-name" :value (:name e)}])]
-     [:td (:funds e)]
-     [:td
-      (form-to [:get "/increment"]
-               [:div
-                [:input {:name "cat-id" :type :hidden :value (:id e)}]
-                [:input {:name "inc-amount" :type :number}]])]
-
-     [:td
-      (form-to [:get "/decrement"]
-               [:div
-                [:input {:name "cat-id" :type :hidden :value (:id e)}]
-                [:input {:name "dec-amount" :type :number}]])]
-
-     [:td
-      (form-to
-       [:get "/delete-category"]
-       [:input {:name "cat-id" :type :hidden :value (:id e)}]
-       [:button "X"])]
-
-     ,,,]))
-
-
-(defn transaction
-  [t]
-  (let [names (cat-names)]
-    [:tr
-     [:td (names (:categoryid t))]
-     [:td (:amount t)]
-     [:td (:ts t)]
-     [:td (form-to [:post "/delete-transaction"]
-                   [:input {:name "tx-id" :type :hidden :value (:id t)}]
-                   [:button "X"])]]))
-
-
-(defn index
-  [config]
-  (html5
-   [:head [:title "Budget"]]
-   [:body.mui-container
-    [:h1 "Budget"]
-    [:table.mui-table
-     [:thead
-      [:tr [:th "Name"] [:th "Current Funds"] [:th "Earn"] [:th "Spend"] [:th "Delete"]]]
-     [:tbody
-      (for [e (sort-by :name (db/get-categories))]
-        (entry e))]]
-    [:h3 (str "Total: " (-> (db/get-sum) first :sum) " Avail: " avail)]
-    [:div
-     [:h2 "Add new category"]
-     (form-to {:class "add-category"} [:get "/add-category"]
-              [:div.mui-textfield
-               [:input
-                {:name "cat-name" :type :text :placeholder "Category name"}]]
-              [:div
-               [:label "Value "]
-               [:input {:name "funds" :type :number :value 0}]
-               [:button.mui-btn.mui-btn--primary "Add category"]])]
-    [:div
-     [:h2 "Latest Transactions"]
-     [:table.mui-table
-      [:thead
-       [:tr [:th "Name"] [:th "Amount"] [:th "Time"] [:th "Remove"]]]
-      [:tbody
-       (for [t (->> (db/get-transactions) (sort-by :ts) reverse (take n-transactions))]
-         (transaction t))]]]
-    [:div#cljs-target]
-    (apply include-js (:javascripts config))
-    (apply include-css (:styles config))]))
-
 (defn- app-routes
   [config]
   (routes
-   (GET "/" [] (index config))
-   (GET "/add-category" [cat-name funds]
-        (db/add-category cat-name (u/parse-int funds))
-        (redirect "/"))
-   (GET "/increment" [cat-id inc-amount]
-        (db/update-funds
-         (u/parse-int cat-id)
-         (u/parse-int inc-amount)
-         :increment)
-        (redirect "/"))
-   (GET "/decrement" [cat-id dec-amount]
-        (db/update-funds
-         (u/parse-int cat-id)
-         (u/parse-int dec-amount)
-         :decrement)
-        (redirect "/"))
-   (GET "/delete-category" [cat-id]
-        (db/delete-category (u/parse-int cat-id))
-        (redirect "/"))
+   (GET "/" [] (render/index config))
+   (POST "/add-category" [cat-name funds]
+         (db/add-category cat-name
+                          (u/parse-int funds))
+         (redirect "/"))
+   (POST "/increment" [cat-id inc-amount]
+         (db/update-funds (u/parse-int cat-id)
+                          (u/parse-int inc-amount)
+                          :increment)
+         (redirect "/"))
+   (POST "/decrement" [cat-id dec-amount]
+         (db/update-funds (u/parse-int cat-id)
+                          (u/parse-int dec-amount)
+                          :decrement)
+         (redirect "/"))
+   (POST "/delete-category" [cat-id]
+         (db/delete-category (u/parse-int cat-id))
+         (redirect "/"))
    (POST "/delete-transaction" [tx-id]
-        (db/delete-transaction (u/parse-int tx-id))
-        (redirect "/"))
-   (POST "update-name" [cat-id cat-name]
-         (db/update-name (u/parse-int cat-id) cat-name))
+         (db/delete-transaction (u/parse-int tx-id))
+         (redirect "/"))
+   (POST "/update-name" [cat-id cat-name]
+         (db/update-name (u/parse-int cat-id) cat-name)
+         (redirect "/"))
    (r/resources "/")
-   (r/not-found
-    (html5 "not found"))))
+   (r/not-found render/not-found)))
 
 (defn new-handler
   [config]
---
+++ src/clj/budget/render.clj
@@ -0,0 +1,104 @@
+(ns budget.render
+  (:require
+   [budget.db :as db]
+   [hiccup.form :refer [form-to]]
+   [hiccup.page :refer [html5 include-css include-js]]))
+
+
+(def avail 927)
+(def n-transactions 20)
+
+
+(defn fmt-entry
+  [{:keys [name funds]}]
+  (format "%s %s" name funds))
+
+
+(defn entry
+  [e]
+  (let [label (-> e (select-keys [:name :funds]) fmt-entry)]
+    [:tr
+
+     [:td
+      (form-to  [:post "/update-name"]
+                [:input {:type :hidden :name "cat-id" :value (:id e)}]
+                [:input {:type :text :name "cat-name" :value (:name e)}])]
+     [:td (:funds e)]
+     [:td
+      (form-to [:post "/increment"]
+               [:div
+                [:input {:name "cat-id" :type :hidden :value (:id e)}]
+                [:input {:name "inc-amount" :type :number}]])]
+
+     [:td
+      (form-to [:post "/decrement"]
+               [:div
+                [:input {:name "cat-id" :type :hidden :value (:id e)}]
+                [:input {:name "dec-amount" :type :number}]])]
+
+     [:td
+      (form-to
+       [:post "/delete-category"]
+       [:input {:name "cat-id" :type :hidden :value (:id e)}]
+       [:button "X"])]
+
+     ,,,]))
+
+
+(defn category-names
+  []
+  (let [cs (db/get-categories)
+        ids (map :id cs)
+        ns  (map :name cs)]
+    (zipmap ids ns)))
+
+
+(defn transaction
+  [t]
+  (let [names (category-names)]
+    [:tr
+     [:td (names (:categoryid t))]
+     [:td (:amount t)]
+     [:td (:ts t)]
+     [:td (form-to [:post "/delete-transaction"]
+                   [:input {:name "tx-id" :type :hidden :value (:id t)}]
+                   [:button "X"])]]))
+
+
+(defn index
+  [config]
+  (html5
+   [:head [:title "Budget"]]
+   [:body.mui-container
+    [:h1 "Budget"]
+    [:table.mui-table
+     [:thead
+      [:tr [:th "Name"] [:th "Current Funds"] [:th "Earn"] [:th "Spend"] [:th "Delete"]]]
+     [:tbody
+      (for [e (sort-by :name (db/get-categories))]
+        (entry e))]]
+    [:h3 (str "Total: " (-> (db/get-sum) first :sum) " Avail: " avail)]
+    [:div
+     [:h2 "Add new category"]
+     (form-to {:class "add-category"} [:get "/add-category"]
+              [:div.mui-textfield
+               [:input
+                {:name "cat-name" :type :text :placeholder "Category name"}]]
+              [:div
+               [:label "Value "]
+               [:input {:name "funds" :type :number :value 0}]
+               [:button.mui-btn.mui-btn--primary "Add category"]])]
+    [:div
+     [:h2 "Latest Transactions"]
+     [:table.mui-table
+      [:thead
+       [:tr [:th "Name"] [:th "Amount"] [:th "Time"] [:th "Remove"]]]
+      [:tbody
+       (for [t (->> (db/get-transactions) (sort-by :ts) reverse (take n-transactions))]
+         (transaction t))]]]
+    [:div#cljs-target]
+    (apply include-js (:javascripts config))
+    (apply include-css (:styles config))]))
+
+
+(def not-found (html5 "not found"))
