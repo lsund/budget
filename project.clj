faae21c3737337b71d95da9343b723a4bd759313
Ludvig Sundström <lud.sund@gmail.com>
2019-04-26 17:37:54 +0200

    Correctlry quote version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject finances 2.10.0-SNAPSHOT
+(defproject finances "2.10.0-SNAPSHOT"
   :description "Fun finance program"
   :url "https://github.com/lsund/finances"
   :min-lein-version "2.7.0"
