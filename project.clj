f874fa84761918c972ad2a74fd0101efe495f10f
Ludvig Sundström <lud.sund@gmail.com>
2019-06-05 20:08:19 +0200

    Remove figwheel

--- project.clj
+++ project.clj
@@ -22,8 +22,7 @@
                  [com.stuartsierra/component "0.3.2"]
                  [slingshot "0.12.2"]
                  [me.lsund/util "0.4.3"]]
-  :plugins [[lein-figwheel "0.5.15"]
-            [environ/environ.lein "0.3.1"]
+  :plugins [[environ/environ.lein "0.3.1"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [io.aviso/pretty "0.1.34"]]
   :source-paths ["src/clj" "src/cljs"]
@@ -33,8 +32,6 @@
   :cljsbuild {:builds
               [{:id "dev"
                 :source-paths ["src/cljs"]
-                :figwheel {:on-jsload "finances.core/on-js-reload"
-                           :open-urls ["http://localhost:3449/index.html"]}
                 :compiler {:main                 finances.core
                            :asset-path           "js/compiled/out"
                            :output-to            "resources/public/js/compiled/finances.js"
@@ -50,10 +47,7 @@
   :figwheel {:css-dirs ["resources/public/css"]}
   :repl-options {:init-ns user
                  :timeout 120000}
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
-                                  [figwheel-sidecar "0.5.15"]
-                                  [com.cemerick/piggieback "0.2.2"]]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]]
                    :source-paths ["src/clj" "src/cljs" "dev"]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
